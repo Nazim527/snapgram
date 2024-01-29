5470fcacd7401b0413ca2aa4465d9367c9ba09f1
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-01-29 16:40:28 +0400

    SignUp UI finish and components update

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,5 +4,6 @@ export const SignUpValidation = z.object({
     name: z.string().min(2, {message: "Too Short"}),
     username: z.string().min(2, {message: "Too Short"}),
     email: z.string().email(),
-    password: z.string().min(8, {message: "Too Short"})
-  });
+    password: z.string().min(8, {message: "Password must be at least 8 characters."})
+});
+    
